f483369857369d15ea3d3b2ff248dfd7c1fb6685
Jabzyt <benjamin.blitz@edu.esiee.fr>
2023-06-21 10:48:05 +0200

    un des derniers poush

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # CaloriesCounter
 
 Calories counter in react native by Nathanaël Bosquet--Lefebvre and Benjamin Blitz
+
+This project was done with the help of React Native documentation available here : https://reactnative.dev/docs/getting-started
+
+You can run the project by first installing all the packages with the command "npm install" or "yarn install" in the project folder.
+Once all the dependencies are installed, use the command "npx expo start" in the terminal while in the project folder.
+
+You can start by filling your personal information in the "Health" tab, remember you have to be at least 15 to use the application.
+
+Once every information is filled, you can check the confirm checkbox and submit so that the application can calculate your Total Caloric Intake.
+
+The next tab is the "Food" tab where you can search for your meals and see how much calories each meal contains.
+By pressing the "+", you can add the meal to the planning.
+
+Speaking of the planning, the third tab is the "Planning" tab, where you can add your meals for each day (by sliding to the day you want and pressing the "+" at the bottom).
+Pressing the "+" will direct you to the "Food" tab where you can search the meal you want to add and will open a picker to chose either the meal is for the breakfast, lunch, snack or dinner. Once you added what you neededed, the total calories of the meal will be shown at the right of the app.
+You can also see the total calories at the top so you can know how many calories you ate for the day.
--- Tabs/HealthGoals.js
+++ Tabs/HealthGoals.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Modal, TouchableWithoutFeedback, Text, Alert } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal, TouchableWithoutFeedback, Text, Alert, Keyboard } from 'react-native';
 import { Picker } from "@react-native-picker/picker";
 import {Checkbox} from "expo-checkbox";
 
@@ -148,8 +148,8 @@ const HealthGoals = () => {
       Alert.alert("Error", "Name should contain at least 3 characters");
       return;
     }
-    if (age < 18) {
-      Alert.alert("Error", "You must be at least 18");
+    if (age < 15) {
+      Alert.alert("Error", "You must be at least 15");
       return;
     }
     if (!isAgreed) {
@@ -171,128 +171,127 @@ const HealthGoals = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        value={name}
-        onChangeText={setName}
-      />
-
-      <View style={styles.row}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Height"
-            value={height}
-            onChangeText={setHeight}
-            keyboardType="numeric"
-          />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+        />
+  
+        <View style={styles.row}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Height"
+              value={height}
+              onChangeText={setHeight}
+              keyboardType="numeric"
+            />
+          </View>
+  
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Weight"
+              value={weight}
+              onChangeText={setWeight}
+              keyboardType="numeric"
+            />
+          </View>
         </View>
-
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Weight"
-            value={weight}
-            onChangeText={setWeight}
-            keyboardType="numeric"
-          />
-        </View>
-      </View>
-
-      <TouchableWithoutFeedback onPress={handleGenderPickerPress}>
-        <View style={styles.pickerContainer}>
-          <Text style={styles.pickerText}>{gender}</Text>
-        </View>
-      </TouchableWithoutFeedback>
-
-      <TextInput
-        placeholder="Age"
-        value={age}
-        onChangeText={setAge}
-        keyboardType="numeric"
-        style={styles.input}
-      />
-
-      <TouchableWithoutFeedback onPress={handleActivityLevelPickerPress}>
-        <View style={styles.pickerContainer}>
-          <Text style={styles.pickerText}>
-            {ActivityLevels.find((level) => level.value === activityLevel)?.name}
+  
+        <TouchableWithoutFeedback onPress={handleGenderPickerPress}>
+          <View style={styles.pickerContainer}>
+            <Text style={styles.pickerText}>{gender}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+  
+        <TextInput
+          placeholder="Age"
+          value={age}
+          onChangeText={setAge}
+          keyboardType="numeric"
+          style={styles.input}
+        />
+  
+        <TouchableWithoutFeedback onPress={handleActivityLevelPickerPress}>
+          <View style={styles.pickerContainer}>
+            <Text style={styles.pickerText}>
+              {ActivityLevels.find((level) => level.value === activityLevel)?.name}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+  
+        <TouchableWithoutFeedback onPress={handleGoalPickerPress}>
+          <View style={styles.pickerContainer}>
+            <Text style={styles.pickerText}>
+              {Goals.find((item) => item.value === goal)?.name}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+  
+        <Button title="Submit" onPress={handleSubmit} />
+  
+        <Modal visible={showGenderPicker} animationType="slide" transparent>
+          <View style={styles.modalContainer}>
+            <Picker
+              selectedValue={gender}
+              onValueChange={handleGenderPickerChange}
+              style={styles.picker}
+            >
+              {GenderChoices.map((choice) => (
+                <Picker.Item key={choice} label={choice} value={choice} />
+              ))}
+            </Picker>
+          </View>
+        </Modal>
+  
+        <Modal visible={showActivityLevelPicker} animationType="slide" transparent>
+          <View style={styles.modalContainer}>
+            <Picker
+              selectedValue={activityLevel}
+              onValueChange={handleActivityLevelPickerChange}
+              style={styles.picker}
+            >
+              {ActivityLevels.map((level) => (
+                <Picker.Item key={level.value} label={level.name} value={level.value} />
+              ))}
+            </Picker>
+          </View>
+        </Modal>
+  
+        <Modal visible={showGoalPicker} animationType="slide" transparent>
+          <View style={styles.modalContainer}>
+            <Picker
+              selectedValue={goal}
+              onValueChange={handleGoalPickerChange}
+              style={styles.picker}
+            >
+              {Goals.map((goal) => (
+                <Picker.Item key={goal.value} label={goal.name} value={goal.value} />
+              ))}
+            </Picker>
+          </View>
+        </Modal>
+  
+        <View style={styles.switchContainer}>
+          <Checkbox value={isAgreed} onValueChange={handleAgreedCheckboxChange} />
+          <Text style={styles.switchText}>
+            I confirm that my information is correct
           </Text>
         </View>
-      </TouchableWithoutFeedback>
-
-      <TouchableWithoutFeedback onPress={handleGoalPickerPress}>
-        <View style={styles.pickerContainer}>
-          <Text style={styles.pickerText}>
-            {Goals.find((item) => item.value === goal)?.name}
-          </Text>
-        </View>
-      </TouchableWithoutFeedback>
-
-      <Button title="Submit" onPress={handleSubmit} />
-
-      <Modal visible={showGenderPicker} animationType="slide" transparent>
-        <View style={styles.modalContainer}>
-          <Picker
-            selectedValue={gender}
-            onValueChange={handleGenderPickerChange}
-            style={styles.picker}
-          >
-            {GenderChoices.map((choice) => (
-              <Picker.Item key={choice} label={choice} value={choice} />
-            ))}
-          </Picker>
-        </View>
-      </Modal>
-
-      <Modal visible={showActivityLevelPicker} animationType="slide" transparent>
-        <View style={styles.modalContainer}>
-          <Picker
-            selectedValue={activityLevel}
-            onValueChange={handleActivityLevelPickerChange}
-            style={styles.picker}
-          >
-            {ActivityLevels.map((level) => (
-              <Picker.Item key={level.value} label={level.name} value={level.value} />
-            ))}
-          </Picker>
-        </View>
-      </Modal>
-
-      <Modal visible={showGoalPicker} animationType="slide" transparent>
-        <View style={styles.modalContainer}>
-          <Picker
-            selectedValue={goal}
-            onValueChange={handleGoalPickerChange}
-            style={styles.picker}
-          >
-            {Goals.map((goal) => (
-              <Picker.Item key={goal.value} label={goal.name} value={goal.value} />
-            ))}
-          </Picker>
-        </View>
-      </Modal>
-      <View style={styles.switchContainer}>
-      <Checkbox value={isAgreed} onValueChange={handleAgreedCheckboxChange} />
-      <Text style={styles.switchText}>
-        I confirm that my informations are correct
-      </Text>
-     </View>
-     {/* {bmr !== null && (
-        <Text style={styles.bmrText}>
-          Your BMR is: {bmr.toFixed(2)} calories per day.
-        </Text>
-      )} */}
-      {calories !== null && (
+  
+        {calories !== null && (
           <Text style={styles.resultText}>
             Total Caloric Intake: {calories.toFixed(2)} calories per day
           </Text>
         )}
-    </View>
-    
+      </View>
+    </TouchableWithoutFeedback>
   );
+  
 };
 
 const styles = StyleSheet.create({
@@ -373,200 +372,3 @@ const styles = StyleSheet.create({
 });
 
 export default HealthGoals;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { StatusBar } from 'expo-status-bar';
-// import { View, TextInput, Text, Switch, Button, Alert, StyleSheet  } from 'react-native';
-// import DateTimePicker from "@react-native-community/datetimepicker";
-// import Checkbox from "expo-checkbox";
-// import React, { useState } from "react";
-// import { Picker } from "@react-native-picker/picker";
-// // import PickerGender from '../managers/PickerManager'
-
-
-// export default function HealthGoals({navigation, route}) {
-//   const [name, setName] = useState("");
-//   const [height, setHeight] = useState("");
-//   const [weight, setWeight] = useState("");
-  
-//   const [gender, setGender] = useState("");
-//   const [dateOfBirth, setDateOfBirth] = useState(new Date());
-//   const [isAgreed, setIsAgreed] = useState(false);
-//   const [age, setAge] = useState("");
-//   const [pickerSelectedValue, setPickerSelectedValue] = useState("");
-
-// const handleNameChange = (text) => {
-//   setName(text);
-// };
-
-// const handleDateOfBirthChange = (event, selectedDate) => {
-//   const currentDate = selectedDate || dateOfBirth;
-//   setDateOfBirth(currentDate);
-//   calculateAge(currentDate);
-// };
-
-// const handleHeightChange = (text) => {
-//   setHeight(text);
-// };
-// const handleWeightChange = (text) => {
-//   setWeight(text);
-// };
-// const handleAgreedCheckboxChange = (value) => {
-//   setIsAgreed(value);
-// };
-// const handleGenderChange = (value) => {
-//   setGender(value);
-// };
-
-// const calculateAge = (birthDate) => {
-//   const today = new Date();
-//   const diff = today - birthDate;
-//   const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
-//   setAge(age.toString());
-// };
-
-// const handleSubmit = () => {
-//   if (!name || name.length < 3) {
-//     Alert.alert("Error", "Name should contain at least 3 characters");
-//     return;
-//   }
-//   if (dateOfBirth > new Date()) {
-//     Alert.alert("Error", "Date of Birth cannot be a future date");
-//     return;
-//   }
-//   if (!isAgreed) {
-//     Alert.alert("Error", "You must agree to the rules before submitting");
-//     return;
-//   }
-
-//   // If all validations pass, display the form information
-//   const message = `Name: ${name}\nDate of Birth: ${dateOfBirth.toDateString()}\nAge: ${age}\nHeight ${height}\nWeight ${weight}
-//   \nGender ${gender}`;
-
-//   Alert.alert("Form Submitted", message);
-// };
-
-// return (
-//   <View style={styles.container}>
-//     <TextInput
-//       style={styles.input}
-//       placeholder="Name"
-//       onChangeText={handleNameChange}
-//       value={name}
-//       maxLength={30}
-//     />
-//     <DateTimePicker
-//         value={dateOfBirth}
-//         mode="date"
-//         onChange={handleDateOfBirthChange}
-//         maximumDate={new Date()}
-//       />
-//     <TextInput
-//       style={styles.input}
-//       placeholder="Height"
-//       onChangeText={handleHeightChange}
-//       value={height}
-//       keyboardType="numeric"
-//       maxLength={3}
-//     />
-//     <TextInput
-//       style={styles.input}
-//       placeholder="Weight"
-//       onChangeText={handleWeightChange}
-//       value={weight}
-//       keyboardType="numeric"
-//       maxLength={2}
-//     />
-//     <Picker
-//             value = {gender}
-//             selectedValue={pickerSelectedValue}
-//             onValueChange={(itemValue, itemIndex) =>
-//               handleGenderChange(itemValue)
-//             }
-//             style={styles.pickerStyle}
-//           >
-//             <Picker.Item label="Male" value="male" />
-//             <Picker.Item label="Female" value="female" />
-//             <Picker.Item label="Other" value="other" />
-//           </Picker>
-//     {/* <View style={styles.switchContainer}>
-//       <Switch value={isDeveloper} onValueChange={handleDeveloperSwitchChange} />
-//       <Text style={styles.switchText}>I'm a great React Native developer</Text>
-//     </View> */}
-    // <View style={styles.switchContainer}>
-    //   <Checkbox value={isAgreed} onValueChange={handleAgreedCheckboxChange} />
-    //   <Text style={styles.switchText}>
-    //     I confirm that my informations are exact
-    //   </Text>
-    //  </View>
-//     <Button title="Submit" onPress={handleSubmit} disabled={!name || !dateOfBirth || !isAgreed} />
-//   </View>
-// );
-// }
-
-// const styles = StyleSheet.create({
-// container: {
-//   flex: 0.70,
-//   alignItems: "center",
-//   justifyContent: "center",
-//   paddingHorizontal: 20,
-// },
-// input: {
-//   width: "70%",
-//   height: 40,
-//   borderWidth: 1,
-//   borderColor: "#ccc",
-//   borderRadius: 5,
-//   marginBottom: 10,
-//   paddingHorizontal: 10,
-// },
-// switchContainer: {
-//   flexDirection: "row",
-//   alignItems: "center",
-//   marginBottom: 10,
-// },
-// switchText: {
-//   marginLeft: 10,
-// },
-// pickerStyle: { width: 200, height: 40, paddingTop: 65 },
-// pickerItemStyle: { color: "black", backgroundColor: "red" },
-// selectButton: {
-//   width: "90%",
-//   backgroundColor: "green",
-//   alignContent: "center",
-//   borderRadius: 10,
-// },
-// });
-// // export default function App() {
-// //   return (
-// //     <View style={styles.container}>
-// //       <Text>C'est les HealthGoals oue ouee</Text>
-// //       <StatusBar style="auto" />
-// //     </View>
-// //   );
-// // }
-
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: '#fff',
-// //     alignItems: 'center',
-// //     justifyContent: 'center',
-// //   },
-// // });
